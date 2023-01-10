f9c67a500203f631b92192f859ead6c487610132
JaviK07 <javiernaim@hotmail.com>
2023-01-09 21:26:20 -0300

    Routing resueltooo

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+function Home () {
+    return (
+        <div>Lorem ipsum dolor sit amet consecte
+            tur adipisicing elit. Eius hic corporis sit temporib
+            us reprehenderit eum debitis, accusamus ab consectetur eligen
+            di, natus exercitationem repellat eaque, earum dolores. Inve
+            ntore eaque accusantium minima.</div>
+    )
+}
+
+export default Home
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/foods.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const foods = () => {
+    return (
+        <div>foods</div>
+    )
+}
+
+export default foods
+
+
+
+
+
+
