9503644ab11b32ebbb487a26bc62b2361997f96a
JaviK07 <javiernaim@hotmail.com>
2023-01-22 16:24:21 -0300

    main done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ function Main() {
     return (
         <div className={styles.Main}>
            <div className={styles.main__div}>
-                <h1 className={styles.Greetings}>Welcome to LittleLemon Restaurant</h1>
+                <h1 className={styles.Greetings}>Welcome to LittleLemon Restaurant!</h1>
                 <p className={styles.greetingsP}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dignissimos enim quia veritatis repellat dolor blanditiis sunt</p>
                 <div className={styles.buttonContainer}>
                     <button className={styles.button1}>Reserve</button>
