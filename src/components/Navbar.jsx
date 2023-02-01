3cb21eb235417ea721b021f599e5a8873b8a6ad8
JaviK07 <javiernaim@hotmail.com>
2023-01-31 21:19:36 -0300

    navbar animation in the making

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import styles from "../styles/Navbar.module.css"
+import MenuAnimated from "./MenuAnimated.jsx";
 
 function Navbar () {
     return (
@@ -11,8 +12,10 @@ function Navbar () {
                     <Link to="#" className={styles.links}> Menu </Link>
                     <Link to="#" className={styles.links}> Contact Us </Link>
                     <Link to="#" className={styles.links}> About Us </Link>
-                    <div className={styles.burguerMenu}>menu</div>
+                    {/* <button className={styles.burguerMenu}><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAS9JREFUSEuNVUESxCAIw/8/2p2yKgEC2ks7akGSEIacZ4jIFPEv28avdYZvnhC6/R0lTxVhyJApU7fXd0qMN/zO7DTTn9RAazMFXQm04vXYlfzligTprwt0WNkTRCRBBXi7viHCeAT+tFQtsH/JmgLLMfUkIwfA5cCYTEWObo+AxzoJ5B/Z8eoSvEBh6qqqDFdCKEyOb63RSdMk3jUaKeleQXaEmIJzQJqLu8fHsbecBeYhUL2ghavXPknQuhbxnBeKAKmoRKyw8cLsOcHOThzvM7FLDE5tv2SCFwthfcAsstZavlCK+aailMJXA7YBCcI8qMaMxc4ab3zlOEnXasluuP9U060cmaSFLuSb9HwyZ62FOz6MmlqygZA90N8qaC+0rAW8mxBI/KeyJFzHen7fq6seB8bggwAAAABJRU5ErkJggg=="/></button> */}
+                    <MenuAnimated />
                     </nav>
+
             </header>
         
     )
