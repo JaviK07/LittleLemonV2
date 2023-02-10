0c01c8faa95561cbd9c7e482b94c331a916aff8a
JaviK07 <javiernaim@hotmail.com>
2023-02-10 19:09:47 -0300

    FooterDone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,29 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function Footer () {
     return (
-        <footer>
+        <footer className={styles.footerContainer}>
+            <div className={styles.footerContainer__div}>
+                <div className={styles.rrssContainer}>
+                    <a href="https://facebook.com" target='_blank'>
+                        <img src={require('../Images/fb.png')} className={styles.rrss} alt="fb" />
+                    </a>
+                    <a href="https://instagram.com" target='_blank'>
+                        <img src={require('../Images/ig.png')} className={styles.rrss} alt="ig" />
+                    </a>
+
+                    <a href="https://twitter.com" target='_blank'>
+                        <img src={require('../Images/tw.png')} className={styles.rrss} alt="tw" />  
+                    </a>
+                </div>
             
-            <div className={styles.rrssContainer}>
-                <img src={require('../Images/fb.png')} className={styles.rrss} alt="fb" />
-                <img src={require('../Images/ig.png')} className={styles.rrss} alt="ig" />
-                <img src={require('../Images/tw.png')} className={styles.rrss} alt="tw" />  
+                <div className={styles.contactContainer}>
+                    <Link to="#" className={styles.contactContainer__links}> Contact Us </Link>
+                    <Link to="#" className={styles.contactContainer__links}> About Us </Link>
+                </div>
+            
+            
+                
             </div>
-            
-            <div>
-                <Link to="#" className={styles.rrss__links}> Contact Us </Link>
-                <Link to="#" className={styles.rrss__links}> About Us </Link>
-            </div>
-            
-            
             <div className={styles.copyrigth}>Javier Kadjo ©</div>
         </footer>
     )
